848d12c411dcdf921a9da2f902084cc3726c9719
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-05-19 20:49:08 +0530

    def is_unique_number(number):     digit_list = list(str(number))     return len(digit_list) == len(set(digit_list))
    # Example usage
    num = int(input())
    if is_unique_number(num):
        print("Unique Number")
    else:
        print("Not Unique Number")

---
+++ Unique_number.py
@@ -0,0 +1,10 @@
+def is_unique_number(number):
+    digit_list = list(str(number))
+    return len(digit_list) == len(set(digit_list))
+
+# Example usage
+num = int(input())
+if is_unique_number(num):
+    print("Unique Number")
+else:
+    print("Not Unique Number")
